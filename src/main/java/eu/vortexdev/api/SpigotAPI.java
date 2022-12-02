ae785d8a54c7cd1de77f3444c758ae16b6f9cc24
xVorttex <sigamatute@gmail.com>
2022-12-02 17:41:58 +0200

    latest

--- src/main/java/eu/vortexdev/api/SpigotAPI.java
+++ src/main/java/eu/vortexdev/api/SpigotAPI.java
@@ -1,5 +1,6 @@
 package eu.vortexdev.api;
 
+import eu.vortexdev.api.knockback.KnockbackProfile;
 import eu.vortexdev.api.protocol.MovementListenerAdapter;
 import eu.vortexdev.api.protocol.PacketListenerAdapter;
 
@@ -7,17 +8,37 @@ import java.util.List;
 
 public class SpigotAPI {
 
+    /**
+     * Get collection of all packet listeners {@link PacketListenerAdapter}
+     *
+     * @return the collection of packet listeners
+     */
     public static List<PacketListenerAdapter> getPacketListeners() {
         return null;
     }
 
+    /**
+     * Get collection of all movement listeners {@link MovementListenerAdapter}
+     *
+     * @return the collection of movement listeners
+     */
     public static List<MovementListenerAdapter> getMovementListeners() {
         return null;
     }
 
+    /**
+     * Add a movement listener adapter {@link MovementListenerAdapter}
+     *
+     * @param listener the movement listener adapter
+     */
     public static void addMovementListener(MovementListenerAdapter listener) {
     }
 
+    /**
+     * Add a packet listener adapter {@link PacketListenerAdapter}
+     *
+     * @param listener the packet listener adapter
+     */
     public static void addPacketListener(PacketListenerAdapter listener) {
     }
 
